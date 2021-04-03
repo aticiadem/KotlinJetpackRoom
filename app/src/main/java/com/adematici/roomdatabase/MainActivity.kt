c773e499387719340e3a777b97e9e063f6914f60
Adem Atici <adematicis41@gmail.com>
2021-04-03 11:34:16 +0300

    Tasarim Yapildi, Kutuphaneler Eklendi.

--- app/src/main/java/com/adematici/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/adematici/roomdatabase/MainActivity.kt
@@ -2,10 +2,27 @@ package com.adematici.roomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adematici.roomdatabase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonKullaniciEkle.setOnClickListener {
+            val isim: String = binding.editTextIsim.text.toString()
+            val soyisim: String = binding.editTextSoyisim.text.toString()
+        }
+
+        binding.buttonKullanicilariGor.setOnClickListener {
+            val isim: String = binding.editTextIsim.text.toString()
+            val soyisim: String = binding.editTextSoyisim.text.toString()
+        }
     }
+
 }
